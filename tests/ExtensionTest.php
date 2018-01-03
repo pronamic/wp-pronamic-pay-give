cc51169f59141673912c7333997b8702e18698bc
Reüel van der Steege <reuel@pronamic.nl>
2018-01-03 10:54:35 +0100

    Happy 2018.

--- tests/ExtensionTest.php
+++ tests/ExtensionTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: WordPress pay Give extension test
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
