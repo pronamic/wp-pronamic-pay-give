ee493033ed24e75c2d89b6a52d697078db32dba6
Reüel van der Steege <reuel@pronamic.nl>
2018-06-19 09:50:45 +0200

    Use renamed Item methods in payment data.

--- src/PaymentData.php
+++ src/PaymentData.php
@@ -111,11 +111,11 @@ class PaymentData extends Core_PaymentData {
 		// Item
 		// We only add one total item, because iDEAL cant work with negative price items (discount)
 		$item = new Item();
-		$item->setNumber( $this->get_order_id() );
-		$item->setDescription( $this->get_description() );
+		$item->set_number( $this->get_order_id() );
+		$item->set_description( $this->get_description() );
 		// @see http://plugins.trac.wordpress.org/browser/woocommerce/tags/1.5.2.1/classes/class-wc-order.php#L50
-		$item->setPrice( give_get_payment_amount( $this->donation_id ) );
-		$item->setQuantity( 1 );
+		$item->set_price( give_get_payment_amount( $this->donation_id ) );
+		$item->set_quantity( 1 );
 
 		$items->addItem( $item );
 
