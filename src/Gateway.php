0f6851e476f23c14ff14ecb86582ad44dec56817
Reüel van der Steege <reuel@pronamic.nl>
2019-11-14 12:12:30 +0100

    Cast JSON encode result to string.

--- src/Gateway.php
+++ src/Gateway.php
@@ -206,7 +206,7 @@ class Gateway {
 				sprintf(
 					/* translators: %s: payment data as JSON */
 					__( 'Payment creation failed before sending buyer to payment provider. Payment data: %s', 'pronamic_ideal' ),
-					wp_json_encode( $payment_data )
+					(string) wp_json_encode( $payment_data )
 				),
 				$donation_id
 			);
