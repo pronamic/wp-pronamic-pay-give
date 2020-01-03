4165f46b20295ac963399c92fb46ccfbb634e8e4
Reüel van der Steege <reuel@pronamic.nl>
2020-01-03 14:39:03 +0100

    Getting ready for version 2.0.4.

--- src/Extension.php
+++ src/Extension.php
@@ -21,7 +21,7 @@ use Pronamic\WordPress\Pay\Payments\Payment;
  * Company: Pronamic
  *
  * @author  Reüel van der Steege
- * @version 2.0.3
+ * @version 2.0.4
  * @since   1.0.0
  */
 class Extension {
