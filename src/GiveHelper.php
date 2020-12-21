11706e3bffba4f146f9ab507600cd0a78bcede69
Remco Tolsma <info@remcotolsma.nl>
2020-12-21 13:24:03 +0100

    Use helpers, more DRY.

--- src/GiveHelper.php
+++ src/GiveHelper.php
@@ -11,8 +11,11 @@
 namespace Pronamic\WordPress\Pay\Extensions\Give;
 
 use Pronamic\WordPress\Pay\Address;
+use Pronamic\WordPress\Pay\AddressHelper;
 use Pronamic\WordPress\Pay\ContactName;
+use Pronamic\WordPress\Pay\ContactNameHelper;
 use Pronamic\WordPress\Pay\Customer;
+use Pronamic\WordPress\Pay\CustomerHelper;
 
 /**
  * Give Helper
@@ -77,146 +80,44 @@ class GiveHelper {
 	 * Get customer from user data.
 	 */
 	public static function get_customer_from_user_info( $user_info, $donation_id ) {
-		$name    = self::get_name_from_user_info( $user_info );
-		$email   = \give_get_payment_user_email( $donation_id );
-		$phone   = null;
-		$user_id = null;
-
-		$customer_data = array(
-			$name,
-			$email,
-			$phone,
-			$user_id,
-		);
-
-		$customer_data = \array_filter( $customer_data );
-
-		if ( empty( $customer_data ) ) {
-			return null;
-		}
-
-		$customer = new Customer();
-
-		$customer->set_name( $name );
-
-		if ( ! empty( $email ) ) {
-			$customer->set_email( $email );
-		}
-
-		if ( ! empty( $phone ) ) {
-			$customer->set_phone( $phone );
-		}
-
-		if ( ! empty( $user_id ) ) {
-			$customer->set_user_id( \intval( $user_id ) );
-		}
-
-		return $customer;
+		return CustomerHelper::from_array( array(
+			'name'    => self::get_name_from_user_info( $user_info ),
+			'email'   => \give_get_payment_user_email( $donation_id ),
+			'phone'   => null,
+			'user_id' => null,
+		) );
 	}
 
 	/**
 	 * Get name from user data.
+	 *
+	 * @return ContactName|null
 	 */
 	public static function get_name_from_user_info( $user_info ) {
-		$first_name = self::get_value_from_array( $user_info, 'first_name' );
-		$last_name  = self::get_value_from_array( $user_info, 'last_name' );
-
-		$name_data = array(
-			$first_name,
-			$last_name,
-		);
-
-		$name_data = \array_filter( $name_data );
-
-		if ( empty( $name_data ) ) {
-			return null;
-		}
-
-		$name = new ContactName();
-
-		if ( ! empty( $first_name ) ) {
-			$name->set_first_name( $first_name );
-		}
-
-		if ( ! empty( $last_name ) ) {
-			$name->set_last_name( $last_name );
-		}
-		
-		return $name;
+		return ContactNameHelper::from_array( array(
+			'first_name' => self::get_value_from_array( $user_info, 'first_name' ),
+			'last_name'  => self::get_value_from_array( $user_info, 'last_name' ),
+		) );
 	}
 
 	/**
 	 * Get address from user info.
+	 *
+	 * @return Address|null
 	 */
 	public static function get_address_from_user_info( $user_info, $donation_id ) {
 		$address_info = self::get_value_from_array( $user_info, 'address' );
 
-		$name         = self::get_name_from_user_info( $user_info );
-		$line_1       = self::get_value_from_array( $address_info, 'line1' );
-		$line_2       = self::get_value_from_array( $address_info, 'line2' );
-		$postal_code  = self::get_value_from_array( $address_info, 'zip' );
-		$city         = self::get_value_from_array( $address_info, 'city' );
-		$state        = null;
-		$country_code = null;
-		$email        = \give_get_payment_user_email( $donation_id );
-		$phone        = null;
-
-		$address_data = array(
-			$name,
-			$line_1,
-			$line_2,
-			$postal_code,
-			$city,
-			$state,
-			$country_code,
-			$email,
-			$phone,
-		);
-
-		$address_data = \array_filter( $address_data );
-
-		if ( empty( $address_data ) ) {
-			return;
-		}
-
-		$address = new Address();
-
-		if ( ! empty( $name ) ) {
-			$address->set_name( $name );
-		}
-
-		if ( ! empty( $line_1 ) ) {
-			$address->set_line_1( $line_1 );
-		}
-
-		if ( ! empty( $line_2 ) ) {
-			$address->set_line_2( $line_2 );
-		}
-
-		if ( ! empty( $postal_code ) ) {
-			$address->set_postal_code( $postal_code );
-		}
-
-		if ( ! empty( $city ) ) {
-			$address->set_city( $city );
-		}
-
-		if ( ! empty( $state ) ) {
-			$address->set_region( $state );
-		}
-
-		if ( ! empty( $country_code ) ) {
-			$address->set_country_code( $country_code );
-		}
-
-		if ( ! empty( $email ) ) {
-			$address->set_email( $email );
-		}
-
-		if ( ! empty( $phone ) ) {
-			$address->set_phone( $phone );
-		}
-
-		return $address;
+		return AddressHelper::from_array( array(
+			'name'         => self::get_name_from_user_info( $user_info ),
+			'line_1'       => self::get_value_from_array( $address_info, 'line1' ),
+			'line_2'       => self::get_value_from_array( $address_info, 'line2' ),
+			'postal_code'  => self::get_value_from_array( $address_info, 'zip' ),
+			'city'         => self::get_value_from_array( $address_info, 'city' ),
+			'region'       => null,
+			'country_code' => null,
+			'email'        => \give_get_payment_user_email( $donation_id ),
+			'phone'        => null,
+		) );
 	}
 }
