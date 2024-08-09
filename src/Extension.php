f40a0028145c3c69f20ec2ab319e6465ed5adf0d
Reüel van der Steege <reuel@pronamic.nl>
2024-08-09 16:29:49 +0200

    Happy 2024.

--- src/Extension.php
+++ src/Extension.php
@@ -3,7 +3,7 @@
  * Extension
  *
  * @author    Pronamic <info@pronamic.eu>
- * @copyright 2005-2023 Pronamic
+ * @copyright 2005-2024 Pronamic
  * @license   GPL-3.0-or-later
  * @package   Pronamic\WordPress\Pay\Extensions\Give
  */
@@ -19,7 +19,7 @@ use Pronamic\WordPress\Pay\Plugin;
 /**
  * Title: Give extension
  * Description:
- * Copyright: 2005-2023 Pronamic
+ * Copyright: 2005-2024 Pronamic
  * Company: Pronamic
  *
  * @author  Reüel van der Steege
