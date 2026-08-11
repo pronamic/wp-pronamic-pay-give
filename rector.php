69c01717ce8fd5074e354bc98b7671d89437a02e
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2026-08-11 14:34:55 +0200

    Add Rector setup and bump PHPStan v2
    Introduces a new `rector.php` configuration for `src` and `tests` with baseline PHP, type coverage, dead code, and code quality levels set to 0. Updates dev tooling by upgrading `phpstan/phpstan` to `^2.0`, aligning `szepeviktor/phpstan-wordpress` to `^2.0`, adding `rector/rector` as a dev dependency, and exposing a new `composer rector` script.

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Rector
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2026 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Extensions\Give
+ */
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+	->withPaths(
+		[
+			__DIR__ . '/src',
+			__DIR__ . '/tests',
+		]
+	)
+	->withPhpSets()
+	->withTypeCoverageLevel( 0 )
+	->withDeadCodeLevel( 0 )
+	->withCodeQualityLevel( 0 );
